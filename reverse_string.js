44eceaf81770dab861529d3e29b5e220f99fcca7
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-11-07 18:35:46 +0300

    practicing reverse_string with p[ython and javascript

--- reverse_string.js
+++ reverse_string.js
@@ -3,3 +3,8 @@ function reverseString(str) {
     return str.split('').reverse().join('');
 }
 console.log(reverseString("hello"));
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+console.log(reverseString("peace"));
--- reverse_string.py
+++ reverse_string.py
@@ -1,3 +1,11 @@
 def reverse_string(s):
     return s[::-1]
 print(reverse_string("hello"))
+
+def reverse_string(s):
+    return s[::-1]
+print(reverse_string("whitney"))
+
+def reverse_string(s):
+    return s[::-1]
+print(reverse_string("nancy"))
