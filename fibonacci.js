d079af4c66540acf4290c44acb40ceb20dc14ffa
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-11-19 11:26:02 +0300

    handling the base case

---
+++ fibonacci.js
@@ -0,0 +1,6 @@
+// fibonacci sequence solution in js
+function fibonacci(n) {
+    // handle base case 
+    if (n <= 0) return 0; // fibonacci(0) = 0
+    if (n === 1) return 1; // fibonacci(1) = 1
+ }
