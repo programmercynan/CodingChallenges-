5d6f2b995e0a54d0e0223d3d57fb50c0ba9305d5
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-11-19 12:06:06 +0300

    handling edge cases

---
+++ maximum.js
@@ -0,0 +1,8 @@
+// function to find the maximum value in an array 
+function findMax(arr) {
+    //handle edge case: empty array
+    if (arr.length === 0) {
+        throw new Error("Array is empty. Cannot find maximum value.");
+    }
+    
+}
